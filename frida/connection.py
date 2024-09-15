3ad6e7e79d8192506d5bd7046873fd3315e95e9c
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-09-15 07:40:48 -0600

    Refactor get_crops_recommendation function and add getCrops_time endpoint

--- frida/connection.py
+++ frida/connection.py
@@ -9,9 +9,11 @@ client = OpenAI(
     base_url="http://198.145.126.109:8080/v1"
 )
 
-@app.route('/crops', methods=['GET'])
+@app.route('/getCrops_text', methods=['GET'])
 def get_crops_recommendation():
-    system_message = "You are an agricultural assistant that, based off of sensor data provided, will give the user a recommendation based off the sensor data provided"
+    recommended_time = get_crops_time_recommendation(request.get_json())
+
+    system_message = f"You are an agricultural assistant that, based off of sensor data provided, will give the user a recommendation based off the sensor data provided, and the new time for the crops to be watered: {recommended_time}."
     data = request.get_json()  
     
     response = client.chat.completions.create(
@@ -19,6 +21,36 @@ def get_crops_recommendation():
         messages=[
             {"role": "system", "content": system_message},
             {"role": "user", "content": json.dumps(data)},
+        ],
+        max_tokens=1024,  # Increase token limit to allow longer responses
+        stream=False
+    )
+
+    sprinklers = next(item['sprinklers'] for item in data if 'sprinklers' in item)
+
+    recommended_time = [recommended_time] * sprinklers
+
+    return jsonify({
+        "recommended_time": recommended_time,
+        "response": response.choices[0].message.content
+    })
+
+@app.route('/getCrops_time', methods=['GET'])
+def get_crops_time_recommendation(function_data=None):
+    system_message = "You are an agricultural assistant that, based off of sensor data provided, will give the user ONLY a new time OR COMMA SEPARATED TIMES IF USEFUL for the crops to be watered, NO TEXT"
+    
+    data = None
+    if function_data is None:
+        data = request.get_json()
+    else:
+        data = function_data
+    
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": json.dumps(data)},
+            {"role": "assistante", "content": "HH:MM"},
         ],
         max_tokens=1024,  # Increase token limit to allow longer responses
         stream=False
